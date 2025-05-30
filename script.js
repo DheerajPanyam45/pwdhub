2e0dd3a4b3dacd29873841f261e9189e0207f848
DheerajPanyam45 <dheeraj.panyam@gmail.com>
2025-05-30 14:10:09 +0530

    c=max complted last finishing remaining

--- script.js
+++ script.js
@@ -1,26 +1,76 @@
+function maskPasswrod(pass){
+    let str = ""
+    for(let index =0;index<pass.length;index++){
+        str += "*"
+    }
+    return str;
+}
+
+
+function copyText(txt){
+    
+    navigator.clipboard.writeText(txt).then(
+
+    ()=> {
+        alert("Copy Done!!");
+    },
+    ()=>{
+        alert("clipboard copying failed")
+    },
+);
+}
+
+const deletePassword=(website)=>{
+    let data = localStorage.getItem("passwords")
+    let arr = JSON.parse(data);
+    arrUpdated = arr.filter((e)=>{
+        return e.website != website
+    })
+    localStorage.setItem("passwords", JSON.stringify(arrUpdated))
+    alert(`Successfully delted ${website}'s password`)
+    showPasswords();
+}
+
+
+
 const showPasswords = ()=>{
 
 let tb = document.querySelector("table");
 let data = localStorage.getItem("passwords")
-if (data == null) {
+if (data == null || JSON.parse(data).length==0) {
     tb.innerHTML = "no data to be shown"
 }
 else {
+    tb.innerHTML = ` <tr>
+                <th>Website</th>
+                <th>Username</th>
+                <th>Password</th>
+                <th>Delete</th>
+            </tr>`
     let arr = JSON.parse(data);
     let str =""
     for (let index = 0; index < arr.length; index++) {
         const element = arr[index];
 
         str += `<tr>
-<td>${element.website}</td>
-<td>${element.username}</td>
-<td>${element.password}</td>
-<td>${"Delete"}</td>
+<td>${element.website} 
+    <img src="copy.svg" alt="copy" width="100" height="100" onclick="copyText('${element.website}')"> 
+</td>
+<td>${element.username} 
+    <img src="copy.svg" alt="copy" width="100" height="100" onclick="copyText('${element.username}')">
+</td>
+<td>${maskPasswrod(element.password)} 
+    <img src="copy.svg" alt="copy" width="100" height="100" onclick="copyText('${element.password}')">
+</td>
+<td><button class ="btnsm" onclick="deletePassword('${element.website}')"> Delete</td>
 </tr>`
     }
     tb.innerHTML = tb.innerHTML + str
 
 }
+website.value = "";
+password.value = "";
+username.value = "";
 
 
 }
@@ -41,7 +91,7 @@ document.querySelector(".btn").addEventListener("click", (e) => {
         let json = JSON.parse(localStorage.getItem("passwords"))
         json.push({website:website.value, username: username.value, password: password.value });
         alert("password saved")
-        localStorage.setItem("password", JSON.stringify(json))
+        localStorage.setItem("passwords", JSON.stringify(json))
 
     }
     showPasswords();
